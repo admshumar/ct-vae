9346ac0881c5c18b6f1a42e39990f9c730097d4a
admshumar <admshumar@gmail.com>
2020-03-15 12:44:06 +0100

    add arguments

--- main.py
+++ main.py
@@ -19,6 +19,8 @@ parser.add_argument("--dec_activation", help="Decoder activation function. (Defa
                     type=str, default='relu')
 parser.add_argument("--early_stop", help="Train with early stopping. (Default: True)",
                     type=bool, default=True)
+parser.add_argument("--early_stop_patience", help="Early stopping patience. (Default: 20)",
+                    type=int, default=20)
 parser.add_argument("--enc_activation", help="Encoder activation function. (Default: 'relu')",
                     type=str, default='relu')
 parser.add_argument("--final_activation", help="Final activation function. (Default: 'sigmoid')",
@@ -31,6 +33,8 @@ parser.add_argument("--latent_dim", help="Dimension of the model's latent space.
                     type=int, default=2)
 parser.add_argument("--learning_rate", help="Learning rate. (Default: 1e-4)",
                     type=float, default=1e-4)
+parser.add_argument("--lr_plateau_patience", help="Factor for reducing learning rate upon plateau. (Default: 0.1)",
+                    type=float, default=0.1)
 parser.add_argument("--sgd", help="Train with stochastic gradient descent. (Default: True)",
                     type=bool, default=True)
 args = parser.parse_args()
@@ -39,12 +43,14 @@ os.environ["CUDA_VISIBLE_DEVICES"] = str(args.gpu)
 vae = ConvolutionalVAE(is_mnist=args.is_mnist,
                        number_of_epochs=args.epochs,
                        enable_early_stopping=args.early_stop,
+                       early_stopping_patience=args.early_stop_patience,
                        enable_logging=args.logging,
                        enable_stochastic_gradient_descent=args.sgd,
                        encoder_activation=args.enc_activation,
                        decoder_activation=args.dec_activation,
                        final_activation=args.final_activation,
                        learning_rate_initial=args.learning_rate,
+                       lr_reduction_patience=args.lr_plateau_patience,
                        beta=args.beta,
                        batch_size=args.batch_size,
                        latent_dimension=args.latent_dim,
